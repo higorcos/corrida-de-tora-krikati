516899853bfe45b92c456b06221643fde9e6b5ab
higorcos <higorcosta2001@outlook.com>
2026-06-15 14:12:38 -0300

    deploy: build output for GitHub Pages

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/p_project/',
-  plugins: [react()],
-  assetsInclude: ['**/*.jpeg', '**/*.jpg', '**/*.png', '**/*.webp'],
-})
