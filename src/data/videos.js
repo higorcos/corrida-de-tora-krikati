13bbdd560db64e2dc092c5f686881ee8e874bd78
higorcos <higorcosta2001@outlook.com>
2026-07-01 01:31:55 -0300

    .

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,113 +1,44 @@
 export const videos = [
-  {
+ {
     id: 1,
     youtubeId: 'T8-4oWEya74',
-    descricao: '(Video: Paulo Ferraz)',
-   
-
-  },
-  {
+    descricao: 'Indígenas no centro da aldeia após finalizarem a corrida. (Vídeo: Paulo Ferraz)',
+},
+{
     id: 2,
     youtubeId: '1nCImlVY2N0',
     titulo: '',
-    descricao: '',
-
-
-  },
-
-  {
-    id: 4,
+    descricao: 'Corredores realizando a corrida de encerramento de luto com a tora de barriguda em sistema de revezamento na MA-280. (Vídeo: Paulo Ferraz)',
+},
+{
+    id: 3,
     youtubeId: 'rJuoFFmhvuk',
     titulo: '',
-    descricao: '',
- 
-  },
-
-  {
-    id: 5,
+    descricao: 'Toras posicionadas e organizadas, aguardando o momento da largada. (Vídeo: Paulo Ferraz)',
+},
+{
+    id: 4,
     youtubeId: 'vz-XOhUjcIU',
     titulo: '',
-    descricao: '',
-   
-  },
-  {
-    id: 6,
+    descricao: 'Fogueira acesa durante a cantoria e a dança noturna no pátio da aldeia. (Vídeo: Paulo Ferraz)',
+},
+{
+    id: 5,
     youtubeId: '5Ydq5Xu7TIY',
     titulo: '',
-    descricao: '',
-  
-  },
-  {
-    id: 7,
+    descricao: 'Homens correndo em grande círculo ao redor das mulheres e da fogueira durante o ritual. (Vídeo: Paulo Ferraz)',
+},
+{
+    id: 6,
     youtubeId: 'QIgHosUnOhM',
     titulo: '',
-    descricao: '',
-   
-   
-  },
-    {
-    id: 8,
+    descricao: 'Homens entoando cântico sagrado. (Vídeo: Paulo Ferraz)',
+},
+{
+    id: 7,
     youtubeId: 'gXNt1t3R1Co',
     titulo: '',
-    descricao: '',
-   
-  },
-  /*
-    {
-    id: 8,
-    youtubeId: 'tPHD8sWxY8g',
-    titulo: '',
-    descricao: '',
-    data: 'Junho de 2026',
-    duracao: '',
-  },
-    {
-    id: 9,
-    youtubeId: 'DaDjX1F4wIU',
-    titulo: '',
-    descricao: '',
-    data: 'Junho de 2026',
-    duracao: '',
-  },
-  {
-    id: 10,
-    youtubeId: '0nG_Dv3rw8w',
-    titulo: '',
-    descricao: '',
-    data: 'Junho de 2026',
-    duracao: '',
-  },
-  {
-    id: 11,
-    youtubeId: 'mDXtGLgOCag',
-    titulo: '',
-    descricao: '',
-    data: 'Junho de 2026',
-    duracao: '',
-  },
-  {
-    id: 12,
-    youtubeId: '_Ow2XsT1TJ4',
-    titulo: '',
-    descricao: '',
-    data: 'Junho de 2026',
-    duracao: '',
-  },
-  {
-    id: 13,
-    youtubeId: 'NcFfV-XXsSk',
-    titulo: '',
-    descricao: '',
-    data: 'Junho de 2026',
-    duracao: '',
-  },
-  {
-    id: 14,
-    youtubeId: '-Kldrp0ZBL8',
-    titulo: '',
-    descricao: '',
-    data: 'Junho de 2026',
-    duracao: '',
-  },*/
+    descricao: 'Crianças dando a largada na corrida com a tora menor. (Vídeo: Paulo Ferraz)',
+},
 
 ];
