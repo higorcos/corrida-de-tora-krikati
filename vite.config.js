bccfa0a3a15c1313d9190081682c73b0c53c66c2
higorcos <higorcosta2001@outlook.com>
2026-07-03 23:22:46 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  base: '/p_project/',
+  base: '/corrida-de-tora-krikati/',
   plugins: [
     react(),
     /*
